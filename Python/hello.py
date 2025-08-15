67971ba43adc73f1e8d47587bd908cbfd9c54dfe
Nathan Mok <193081@cloud.rangitoto.school.nz>
2025-08-15 14:59:34 +1200

    Yes

--- Python/hello.py
+++ Python/hello.py
@@ -1,7 +1,1 @@
-print("Hello world!")
-import tkinter as tk
-root = tk.Tk()
-root.title("Hello World Window")
-label = tk.Label(root, text="Hello, World!")
-label.pack()
-root.mainloop()
+print('Hello World!')
