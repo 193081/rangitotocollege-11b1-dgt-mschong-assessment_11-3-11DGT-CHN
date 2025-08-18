71a59166bff2b2b5863b7fc17e85d7df99ab363d
Nathan Mok <193081@cloud.rangitoto.school.nz>
2025-08-19 09:53:44 +1200

    Full screen test

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+import tkinter as tk
+import random
+
+screen = tk.Tk()
+screen.attributes('-fullscreen', True)
+
+block
+def key_press(event):
+    if event.keysym == 'Escape':
+        screen.attributes('-fullscreen', False)
+        quit()
+
+screen.bind('<KeyPress>', key_press)
+screen.mainloop()
