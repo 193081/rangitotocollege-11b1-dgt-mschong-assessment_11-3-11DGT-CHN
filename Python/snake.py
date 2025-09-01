40fcd9d13e959a3127aea4b8e3a9cfe4989c764a
Nathan Mok <193081@cloud.rangitoto.school.nz>
2025-09-01 12:49:25 +1200

    work on snake

--- Python/snake.py
+++ Python/snake.py
@@ -1,5 +1,24 @@
-import random
+from random import *
+from turtle import *
+import time
 
 def start_snek():
-    print('Loading snake...')
+
+    delay = 0.1
+    score = 0
+    high_score = 0
+
+    wn = Screen()
+    wn.title("Snake with Turtle")
+    wn.bgcolor("black")
+    wn.setup(width=600, height=600)
+    wn.tracer(0)
+
+    head = Turtle()
+    head.speed(0)
+    head.shape('Square')
+    head.color('green')
     
+
+    while True:
+        wn.update()
