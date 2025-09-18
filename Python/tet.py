2803e5b790bb99bb0d5d5b0ceeb53a0c1bc51d87
Nathan Mok <193081@cloud.rangitoto.school.nz>
2025-09-18 13:35:11 +1200

    Moving blocks

--- Python/tet.py
+++ Python/tet.py
@@ -1,8 +1,6 @@
 import random
 from turtle import *
 import time
-from turtle import *
-import time
 
 def tet_start():
     delay = 0.1
@@ -11,13 +9,12 @@ def tet_start():
     wn = Screen()
     wn.title("Tetris with Turtle")
     wn.bgcolor("black")
-    wn.setup(width = 800, height = 600)
+    wn.setup(width = 0.9, height = 1.0)
     wn.tracer(0)
 
     pen = Turtle()
     pen.speed(0)
-    while False:
-        pen.hideturtle()
+    pen.hideturtle()
 
     class Tetromino():
         COLOR_MAP = {
@@ -67,10 +64,52 @@ def tet_start():
             pen.end_fill()
     
     tetromino = Tetromino(random.choice(list(Tetromino.COLOR_MAP.keys())))
-    tetromino.print_tetromino()
-    tetromino.render(0, 0, pen)
 
-    wn.update()
+    pos_x = 0
+    pos_y = 200
+
+    BOARD_COLS = 10
+    BOARD_ROWS = 20
+
+    def draw_grid(pen):
+        pen.color("gray")
+        # vertical lines
+        for x in range(BOARD_COLS + 1):
+            pen.penup()
+            pen.goto(-BOARD_COLS * Tetromino.BLOCK_SIZE // 2 + x * Tetromino.BLOCK_SIZE,
+                    -BOARD_ROWS * Tetromino.BLOCK_SIZE // 2)
+            pen.pendown()
+            pen.goto(-BOARD_COLS * Tetromino.BLOCK_SIZE // 2 + x * Tetromino.BLOCK_SIZE,
+                    BOARD_ROWS * Tetromino.BLOCK_SIZE // 2)
+
+        # horizontal lines
+        for y in range(BOARD_ROWS + 1):
+            pen.penup()
+            pen.goto(-BOARD_COLS * Tetromino.BLOCK_SIZE // 2,
+                    -BOARD_ROWS * Tetromino.BLOCK_SIZE // 2 + y * Tetromino.BLOCK_SIZE)
+            pen.pendown()
+            pen.goto(BOARD_COLS * Tetromino.BLOCK_SIZE // 2,
+                    -BOARD_ROWS * Tetromino.BLOCK_SIZE // 2 + y * Tetromino.BLOCK_SIZE)
+
+    def game_loop():
+        nonlocal pos_y, tetromino
+
+        pen.clear()
+        draw_grid(pen)
+        tetromino.render(pos_x, pos_y, pen)
+
+        # drop piece
+        pos_y -= Tetromino.BLOCK_SIZE
+
+        # bottom of the board
+        if pos_y < -(BOARD_ROWS // 2) * Tetromino.BLOCK_SIZE + 20:
+            tetromino = Tetromino(random.choice(list(Tetromino.COLOR_MAP.keys())))
+            pos_y = (BOARD_ROWS // 2) * Tetromino.BLOCK_SIZE
+
+        wn.update()
+        wn.ontimer(game_loop, 500)
+
+    game_loop()
     wn.mainloop()
 
     time.sleep(delay)
