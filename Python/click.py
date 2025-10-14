665fd522ff1d9f39c44c78952b03dc6d07b5e8c6
Nathan Mok <193081@cloud.rangitoto.school.nz>
2025-10-15 09:38:05 +1300

    Upgrade button on the side, not functional yet.

--- Python/click.py
+++ Python/click.py
@@ -5,8 +5,10 @@ def click_check(x, y):
 
 def click_start():
     score = 0
+    increment = 1
 
     screen = Screen()
+    screen.tracer(0)
     screen.title("Clicker Simulator")
     screen.bgcolor("lightblue")
     screen.setup(width=1000, height=800)
@@ -31,17 +33,41 @@ def click_start():
     button.circle(50)
     button.end_fill()
 
+    upgrade = Turtle()
+    upgrade.width(3)
+    upgrade.hideturtle()
+    upgrade.speed(0)
+    upgrade.color('black')
+    upgrade.fillcolor('white')
+    upgrade.penup()
+    upgrade.goto (200, -50)
+    upgrade.pendown()
+    upgrade.begin_fill()
+    for _ in range(2):
+        upgrade.forward(100)
+        upgrade.right(90)
+        upgrade.forward(50)
+        upgrade.right(90)
+    upgrade.end_fill()
+    upgrade.penup()
+    upgrade.goto(250, -50)
+    upgrade.pendown()
+    upgrade.write("Cost: 10", align="center", font=("Consolas", 12, "normal"))
+    upgrade.penup()
+    upgrade.goto(250, -20)
+    upgrade.write("Upgrades", align="center", font=("Consolas", 12, "normal"))
+
     def buttonfunc(x, y):
         nonlocal score
         distance = (x**2 + y**2)**0.5
         if distance <= 50:
             while False:
                 print("Button clicked!")
-            score += 1
+            score += increment
             pen.clear()
             pen.write(f"Score: {score}", align="center", font=("Consolas", 18, "normal"))
     
-    screen.onclick(buttonfunc)
+    screen.onclick(click_check)
     
     def quit_game():
         try:
